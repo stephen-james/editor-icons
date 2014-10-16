22bc7ad05afcb863f193c28f7f95515564684f99
stephen-james <stephenjames@me.com>
2014-10-16 17:11:34 +0100

    cleaned up icons, objects to paths, wip: build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,20 +3,34 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    
+    copy: {
+      main: {
+        src: 'src/editor-icons_*.svg',
+        dest: '*.svg',
+      },
+    },
+
     webfont: {
       icons: {
         src: 'src/*.svg',
         dest: 'build',
         options: {
           types: 'woff',
-          embed: true,
-          engine: 'node'
+          embed: false,
+          engine: 'node',
+          templateOptions: {
+            baseClass: 'e',
+            classPrefix: 'icon-'
+          },
+          font: 'editor-icons'
         }
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-webfont');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   // Default task(s).
   grunt.registerTask('default', ['webfont']);
